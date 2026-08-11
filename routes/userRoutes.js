2dce297d790f1438eb50c2834a8bccdaae43ae3c
FelipeLorini <felipelorini11@gmail.com>
2026-08-10 23:22:31 -0300

    Fix import paths in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const CadastroController = require('../Controllers/CadastroController');
-const LoginController = require('../Controllers/LoginController');
-const ServicoController = require('../Controllers/ServicoController');
-const AgendamentoController = require('../Controllers/AgendamentoController');
+const CadastroController = require('../controllers/CadastroController');
+const LoginController = require('../controllers/LoginController');
+const ServicoController = require('../controllers/ServicoController');
+const AgendamentoController = require('../controllers/AgendamentoController');
 const { auth, verificarPrestador, verificarCliente } = require('../middleware/auth');
 
 router.post('/cadastro', CadastroController.cadastrar);
